a18f6114ea2506164f84e9f59b51de9b987a658f
grenudi <idunerg@gmail.com>
2018-06-07 03:13:20 +0300

    minor cleaning

--- content/content.js
+++ content/content.js
@@ -6,12 +6,11 @@ let area = $("textarea");
 function select(){
 	$("textarea").addClass("selector");
 	$("textarea").click((e)=>{
-		area.removeClass("selector");
+		area.removeClass("selector");//don't highlight :hover
 		area.off("click");
-		let parentlist = $( e.target ).parents()
+		let superSelector = $( e.target )
+			.parents()
 			.map(function() {
-				console.log(this);
-				console.log("CLASS:",$(this).attr("class"));
 				let ids = this.id ? (this.id.split(" ").length > 1 ? "" : "#"+this.id.split(" ")[0]) : "";//more than one id is invalid
 				let classes = $(this).attr("class") ? "."+$(this).attr("class").split(" ").join(".") : "";//select all classes and join them in selector string
 				return this.tagName + ids + classes;
@@ -20,9 +19,7 @@ function select(){
 			.reverse()
 			.join( ">" );
 		console.log(e.target.nodeName);
-		$(parentlist+">"+e.target.nodeName).val("WOW!");
-		// console.log($("body>div>span>div>div").val("SHIT"));
-		console.log(parentlist);
+		$(superSelector+">"+e.target.nodeName).val("WOW!");
 	});
 }
 select();
