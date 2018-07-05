e6922e0225e954f541ddf0aa933934deb1423209
grenudi <idunerg@gmail.com>
2018-07-05 06:44:52 +0300

    creating ugly private properties

--- api/data/model.js
+++ api/data/model.js
@@ -12,26 +12,32 @@
 - update name, path
 - delete
 */
-let ig = function(init){
-  this = Object.assign(this,init);
-  this.list = [];
+let ig = (function(){
+  function ig(init){
+    let list = [];
+    this = Object.assign(this,init);
 
-  this.sync = function(){
-    chrome.storage.sync.set({ig: {list: this.list}});
+    this.sync = function(){
+      chrome.storage.sync.set({ig: {list}});
+    }
+    this.isConflict = function(obj){
+      return list.find((x)=>x.name === obj.name || x.path === obj.path)
+    }
+    this.add = function(name , path , domain){
+      return new Promise((ok,notok)=>{
+        if(list.find((x)=>x.name === name || x.path === path)){
+          notok();
+          return
+        }
+        list.push({name,path,domain});
+        this.sync();
+        ok();
+      })
+    }
   }
-  this.isConflict = function(obj){
-    return this.list.find((x)=>x.name === obj.name || x.path === obj.path
-  }
-  this.add = function(name , path , domain){
-    return new Promise((ok,notok)=>{
-      if(this.list.find((x)=>x.name === name || x.path === path)){
-        notok();
-        return
-      }
-      this.list.push({name,path,domain});
-      this.sync();
-      ok();
-    })
-  }
+  return ig;
+}());
+function(init){
+
 }
 let sg = [];
